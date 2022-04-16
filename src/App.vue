55a982ff59d75d801df07a5cbc5d619a9cde70a7
umudik <seyitumutbicer@gmail.com>
2022-04-16 04:45:52 +0300

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
-  v-app
-    v-app-bar( app color="primary"     dark)
-    v-main yo
+v-app(app)
+  v-app-bar(app color="white" elevate-on-scroll)
+    v-container
+      v-row(align="center")
+          v-app-bar-nav-icon 
+            v-img(src="./assets/logo.png" max-height="32" contain)
+          v-app-bar-title() Fookie.js DOC
+          v-spacer      
+  v-main(app)
+    v-container(app)
       router-view
-</template>
-
-<script>
-export default {
-  name: "App",
-  data: () => ({
-    //
-  }),
-};
-</script>
+  v-footer(padless)
+    v-card(flat tile width="100%" class="tw-text-center")
+      v-card-text
+        | {{ new Date().getFullYear() }} &mdash; 
+        strong Fookie JS
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,31 @@
 <template lang="pug">
-  div hi
+  div 
+    pre 
 </template>
 
 <script>
-
-  export default {
-    name: 'Home',
-
-    components: {
-
-    },
-  }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      url: "http://localhost:2626",
+      token: "admin",
+      models: [],
+    };
+  },
+  mounted: async function () {
+    let token = this.token;
+    let url = this.url;
+    let res = await axios.post(url, {
+      token,
+      model: "model",
+      method: "read",
+    });
+    console.log(res);
+    if (res.data.status) {
+      this.models = res.data;
+    } else {
+    }
+  },
+};
 </script>
